80350c75c18a6f14fbce781921a7127961f524d2
Yzabella Miranda <100642769+redjsun@users.noreply.github.com>
2023-12-15 22:26:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
+  question.classList.add("shake");
   question.innerHTML = "EBA, te vejo dia 16!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
